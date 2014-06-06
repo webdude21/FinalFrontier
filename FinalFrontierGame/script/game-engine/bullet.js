a6380f5d9cbebfbdc15aba0efa102dbd62fd9e93
lostm1nd <kliomba@abv.bg>
2014-06-06 17:58:31 +0300

    firing bullets but they travel slow and stop at some point

--- FinalFrontierGame/script/game-engine/bullet.js
+++ FinalFrontierGame/script/game-engine/bullet.js
@@ -1,4 +1,4 @@
-function Bullet(originX, originY) {
+function Bullet(originX, originY, target) {
 	'use strict';
 	SpaceObject.call(this, {
 		rotation: 0,
@@ -8,13 +8,34 @@ function Bullet(originX, originY) {
 			image: GAME_ART.BULLET,
 			width: GAME_ART.BULLET.width,
 			height: GAME_ART.BULLET.height,
-			speed: 8
+			speed: 26
 		})
 	});
+
+	this.target = target;
+
+	this.move = function() {
+		var directionX = this.target.x - this.getLocation().x;
+		var directionY = this.target.y - this.getLocation().y;
+
+		var denominator = Math.sqrt(directionX*directionX + directionY*directionY);
+
+		var normalizedX = directionX / denominator;
+		var normalizedY = directionY / denominator;
+
+		this.visual.move({
+	            x: normalizedX,
+	            y: normalizedY
+	        });
+	};
+
+	this.update = function update() {
+		this.move();
+	};
 }
 
 Bullet.prototype = new SpaceObject();
 Bullet.prototype.constructor = Bullet;
-Bullet.prototype.update = function update() {
-	this.move(this.speed);
-};
+
+
+
--- FinalFrontierGame/script/user-controls/attachMouseControl.js
+++ FinalFrontierGame/script/user-controls/attachMouseControl.js
@@ -7,8 +7,20 @@ function attachMouseControl(args) {
         event = event || window.event;
         var objPosition = args.controllableObj.getCenterPoint();
         var xDist = event.layerX - objPosition.x;
-        var yDist = (event.layerY - objPosition.y);
+        var yDist = event.layerY - objPosition.y;
         var angle = Math.atan2(yDist, xDist) * (180 / Math.PI);
         controllableObj[rotationInterface](angle);
+    }, false);
+
+    objectHandler.addEventListener('mousedown', function (event) {
+        event = event || window.event;
+        var objManager = new SpaceObjectManager();
+        var objPosition = args.controllableObj.getLocation();
+        var bullet = new Bullet(objPosition.x, objPosition.y, {
+            x: event.layerX,
+            y: event.layerY
+        });
+        
+        objManager.add(bullet);
     }, false);
 }
