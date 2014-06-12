90fc3eab0730b85e2d1c8ef66ce18db25912b9ef
Димо Петров <webdude@webdude.eu>
2014-06-12 16:29:06 +0300

    Implemented level display

--- FinalFrontierGame/script/game-engine/statistics-manager.js
+++ FinalFrontierGame/script/game-engine/statistics-manager.js
@@ -1,4 +1,5 @@
 var STATS_MANAGER = function statistics(args) {
+    var lineSpacing = 5;
 
     var lives = new Kinetic.Text({
         x: args.x,
@@ -22,7 +23,7 @@ var STATS_MANAGER = function statistics(args) {
 
     var score = new Kinetic.Text({
         x: args.x,
-        y: args.y + lives.getTextHeight() + 10,
+        y: args.y + lives.getTextHeight() + lineSpacing,
         text: 'Score:',
         fontSize: args.fontSize,
         fontWeight: args.fontWeight,
@@ -32,8 +33,28 @@ var STATS_MANAGER = function statistics(args) {
 
     var scoreCount = new Kinetic.Text({
         x: args.x + score.getTextWidth() + 10,
-        y: args.y + lives.getTextHeight() + 10,
+        y: args.y + lives.getTextHeight() + lineSpacing,
         text: args.player.getScore(),
+        fontSize: args.fontSize,
+        fontWeight: args.fontWeight,
+        fontFamily: args.fontFamily,
+        fill: args.fillColor
+    });
+
+    var level = new Kinetic.Text({
+        x: args.x,
+        y: args.y + score.getTextHeight() * 2 + lineSpacing * 2,
+        text: 'Level:',
+        fontSize: args.fontSize,
+        fontWeight: args.fontWeight,
+        fontFamily: args.fontFamily,
+        fill: args.fillColor
+    });
+
+    var levelCount = new Kinetic.Text({
+        x: args.x + level.getTextWidth() + 10,
+        y: args.y + score.getTextHeight() * 2 + lineSpacing * 2,
+        text: FINAL_FRONTIER_LEVEL,
         fontSize: args.fontSize,
         fontWeight: args.fontWeight,
         fontFamily: args.fontFamily,
@@ -44,12 +65,14 @@ var STATS_MANAGER = function statistics(args) {
     args.layer.add(livesCount);
     args.layer.add(score);
     args.layer.add(scoreCount);
-    args.layer.draw();
+    args.layer.add(level);
+    args.layer.add(levelCount);
 
     return {
         update: function update() {
             livesCount.text(args.player.getLives());
             scoreCount.text(args.player.getScore());
+            levelCount.text = FINAL_FRONTIER_LEVEL;
             args.layer.draw();
         }
     };
